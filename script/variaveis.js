6c48f20ea8dc49cbf2b93ac11fe38104c90a3d3f
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-04-03 20:06:55 -0300

    Implementado declaração e alerta de variáveis

---
+++ script/variaveis.js
@@ -0,0 +1,14 @@
+// string
+var nome = "Estevão";
+// number
+var idade = 19;
+// boolean
+var estudante = true;
+// float
+var altura = 1.80;
+
+function exibirDados() {
+    alert("Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura + "\nEstudante: " + estudante);
+}
+
+exibirDados();
